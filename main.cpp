6c14dd5f1c7a0198d8ca7fe3342bcbccea61c350
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2020-10-11 11:23:33 +0200

    Added show unloged user menu

--- main.cpp
+++ main.cpp
@@ -4,6 +4,4 @@ using namespace std;
 
 int main() {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy2.txt");
-    ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
 }
